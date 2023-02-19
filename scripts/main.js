9c436be3aa063f257a9269512619e4bf6d73a5d3
Michael Kolesidis <michael.kolesidis@gmail.com>
2023-02-20 01:16:09 +0200

    favicon and source link

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
+// Copyright (c) 2023 Michael Kolesidis (michael.kolesidis@gmail.com)
+// Licensed under the GNU Affero General Public License v3.0.
+// https://www.gnu.org/licenses/gpl-3.0.html
+
 const butt = document.getElementById("butt");
 const fart = document.getElementById("fart");
 
 butt.addEventListener("click", () => {
-  const fartSound = new Audio("./assets/fart.mp3");
+  const fartSound = new Audio("./assets/sounds/fart.mp3");
   fartSound.play();
   setTimeout(() => {
     fart.style.opacity = 1;
